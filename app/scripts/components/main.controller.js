791ff5916642580edb6619d4bb7324cbdc69e116
Sahil Lamba <sahil.lamba95@gmail.com>
2015-01-16 03:04:34 +0530

    Navigation column now working

--- app/scripts/components/main.controller.js
+++ app/scripts/components/main.controller.js
@@ -8,64 +8,85 @@ app.controller('YaadeinController', ['$scope', function ($scope) {
 
 	$scope.navigationItems = [
 		{
-			'id': 'more',
-			'classes': 'icon icon-three-bars active'
+			'id': 'default',
+			'class': 'icon icon-three-bars'
 		},
 		{
 			'id': 'search',
-			'classes': 'icon icon-search'
+			'class': 'icon icon-search-1'
 		},
 		{
 			'id': 'post',
-			'classes': 'icon icon-compose'
+			'class': 'icon icon-compose'
 		},
 		{
 			'id': 'notifications',
-			'classes': 'icon icon-comment-quotes'
+			'class': 'icon icon-store'
 		}
 	];
 
+	$scope.currentNavItem = $scope.navigationItems[0];
+
+	function setCurrentNavItem (navItem) {
+		$scope.currentNavItem = navItem;
+	}
+
+	function isCurrentNavItem (navItem) {
+		return $scope.currentNavItem !== null && $scope.currentNavItem.id === navItem.id;
+	}
+
+	$scope.setCurrentNavItem = setCurrentNavItem;
+	$scope.isCurrentNavItem = isCurrentNavItem;
+
 	$scope.moreOptions = [
+		{
+			'id': 'home',
+			'name': 'Home',
+			'navItem': 'default',
+			'iconClass': 'icon icon-torsos-all',
+			'url': '/'
+		},
 		{
 			'id': 'profile',
 			'name': 'Profile',
-			'iconClasses': 'icon icon-torso'
+			'navItem': 'default',
+			'iconClass': 'icon icon-torso',
+			'url': '/profile'
 		},
 		{
 			'id': 'gallery',
 			'name': 'Gallery',
-			'iconClasses': 'icon icon-camera-2'
+			'navItem': 'default',
+			'iconClass': 'icon icon-device-camera',
+			'url': '/gallery'
 		},
 		{
 			'id': 'settings',
 			'name': 'Settings',
-			'iconClasses': 'icon icon-settings-1'
+			'navItem': 'default',
+			'iconClass': 'icon icon-settings-1',
+			'url': '/settings'
 		},
 		{
-			'id': 'sign-out',
+			'id': 'signOut',
 			'name': 'Sign Out',
-			'iconClasses': 'icon icon-power'
+			'navItem': 'default',
+			'iconClass': 'icon icon-power',
+			'url': '/'
 		}
 	];
 
-	$scope.tickerElements = [
-		{
-			'id': ''
-		}
-	];
-	
 }]);
 
-app.controller('NavigationController', ['$scope', 'NavigationService', function ($scope, NavigationService) {
+// app.controller('NavigationController', ['$scope', 'NavigationService', function ($scope, NavigationService) {
 
-	var makepromise=NavigationService.getData();
-	makepromise.then(function(d){
-		$scope.x=d.forecast;
-	});
+// 	var makepromise=NavigationService.getData();
+// 	makepromise.then(function(d){
+// 		$scope.x=d.forecast;
+// 	});
 
+// }]);
 
-}]);
-
-app.controller('ProfileController',['$routeParams','$scope',function($routeParams,$scope){
-	console.log($routeParams.id);
-}]);
+// app.controller('ProfileController',['$routeParams','$scope',function($routeParams,$scope){
+// 	console.log($routeParams.id);
+// }]);
--- app/scripts/components/main.services.js
+++ app/scripts/components/main.services.js
@@ -2,13 +2,13 @@
 
 var app = angular.module('yaadeinApp');
 
-app.service('NavigationService', ['$http', '$q', function ($http, $q) {
-	var deferred = $q.defer();
-	this.getData = function () {
-		$http.get('/data/forecast.json').success(function (d) {
-			console.log(d);
-			deferred.resolve(d);
-		});
-		return deferred.promise;
-	};
-}]);
+// app.service('NavigationService', ['$http', '$q', function ($http, $q) {
+// 	var deferred = $q.defer();
+// 	this.getData = function () {
+// 		$http.get('/data/forecast.json').success(function (d) {
+// 			console.log(d);
+// 			deferred.resolve(d);
+// 		});
+// 		return deferred.promise;
+// 	};
+// }]);
