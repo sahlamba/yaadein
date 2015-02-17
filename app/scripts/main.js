57760cb41b6989d497c674cbfb707af128391a4b
Sahil Lamba <sahil.lamba95@gmail.com>
2015-02-18 00:15:28 +0530

    Added angular_parallax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('yaadeinApp', ['ngSanitize', 'ngAnimate', 'ngRoute', 'angucomplete-alt', 'ngNotify', 'ngEmoticons', 'angular.filter', 'mentio', 'angularMoment', 'ngNanoscroller', 'directives.skrollr']);
+angular.module('yaadeinApp', ['ngSanitize', 'ngAnimate', 'ngRoute', 'angucomplete-alt', 'ngNotify', 'ngEmoticons', 'angular.filter', 'mentio', 'angularMoment', 'ngNanoscroller']);
