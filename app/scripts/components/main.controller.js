fc5ccc08e39feed4dc208708772cc3f090ee7adb
vaibhav <gvaibhavraj2@gmail.com>
2015-03-17 22:32:49 +0530

    Fixed level 1 nav

--- app/scripts/components/main.controller.js
+++ app/scripts/components/main.controller.js
@@ -10,22 +10,22 @@ app.controller('YaadeinController', ['$scope', '$http', '$q', '$upload', '$locat
 
 	$scope.navigationItems = [
 		{
-			'id': 'home',
-			'class': 'fa fa-home',
-			'url': '/',
-			'hint': 'Home'
-		},
-		{
 			'id': 'profile',
 			'class': 'fa fa-user',
 			'url': '/profile/',
-			'hint': 'My Profile'
+			'hint': 'Profile'
 		},
+    {
+      'id': 'home',
+      'class': 'fa fa-home',
+      'url': '/',
+      'hint': 'Home'
+    },
 		{
 			'id': 'search',
 			'class': 'fa fa-search',
 			'url': '#',
-			'hint': 'Search something'
+			'hint': 'Search'
 		},
 		{
 			'id': 'post',
@@ -33,12 +33,12 @@ app.controller('YaadeinController', ['$scope', '$http', '$q', '$upload', '$locat
 			'url': '#',
 			'hint:': 'Post a memory'
 		},
-		{
-			'id': 'notifications',
-			'class': 'fa fa-bell',
-			'url': '#',
-			'hint': 'Notifications'
-		},
+		//{
+			//'id': 'notifications',
+			//'class': 'fa fa-bell',
+			//'url': '#',
+			//'hint': 'Notifications'
+		//},
 		{
 			'id': 'signOut',
 			'class': 'fa fa-sign-out',
@@ -47,11 +47,12 @@ app.controller('YaadeinController', ['$scope', '$http', '$q', '$upload', '$locat
 		}
 	];
 
-	$scope.currentNavItem = $scope.navigationItems[0];
-	$scope.prevNavItem = $scope.navigationItems[0];
+	$scope.currentNavItem = $scope.navigationItems[1];
 
 	$scope.setCurrentNavItem = function (navItem) {
-		$scope.prevNavItem = $scope.currentNavItem;		
+    if (!navItem) {
+      return;
+    }
 		$scope.currentNavItem = navItem;
 		if ($scope.currentNavItem.id === 'search' || $scope.currentNavItem.id === 'post') {
 			$('#centered').addClass('blur-back');
@@ -74,14 +75,14 @@ app.controller('YaadeinController', ['$scope', '$http', '$q', '$upload', '$locat
 		$('#centered').removeClass('blur-back');
 		$('.right-sidebar').removeClass('blur-back');
 		$('#postBox').fadeOut(300);
-		$scope.currentNavItem = $scope.prevNavItem;
+		$scope.currentNavItem = $scope.navigationItems[1];
 	};
 
 	$scope.closeSearch = function () {
     $('#centered').removeClass('blur-back');
 		$('.right-sidebar').removeClass('blur-back');
 		$('#searchBox').fadeOut(300);
-		$scope.currentNavItem = $scope.prevNavItem;
+    $scope.currentNavItem = $scope.navigationItems[1];
 	};
 
   $scope.addOriginToImageUrl = function (resp) {
@@ -96,7 +97,7 @@ app.controller('YaadeinController', ['$scope', '$http', '$q', '$upload', '$locat
 
   $scope.personSelected = function(selected) {
     $location.path('/profile/' + selected.originalObject.id);
-    $scope.setCurrentNavItem($scope.navigationItems[1]);
+    $scope.setCurrentNavItem();
   };
 
 	$scope.user = {
@@ -108,26 +109,13 @@ app.controller('YaadeinController', ['$scope', '$http', '$q', '$upload', '$locat
 	};
 
 	//Append enrolment number to profile and gallery URLs
-	$scope.navigationItems[1].url += $scope.user.enrolmentNo.toString();
+	$scope.navigationItems[0].url += $scope.user.enrolmentNo.toString();
 
 	$scope.ticks = [];
 	var tickPromise = dataTicker.getTicks();
 	tickPromise.then(function (d) {
 		$scope.ticks = d;
 	});
-
-	$scope.isLoading = false;
-
-	$scope.addToTicker = function () {
-		$scope.isLoading = true;
-		$http.get('http://beta.json-generator.com/api/json/get/HByxuXv')
-			.success(function (ds) {
-				for(var i = 0; i < ds.length; i += 1) {
-					$scope.ticks.push(ds[i]);
-				}
-		});
-		$scope.isLoading = false;
-	}; 
 
   $scope.newPost = {
     'post_owner': $scope.user.name,
